fc614bb9eb50060bdf7906a7b5f04fef7ed1c585
Natallia Pazniak <natalyua_nata_@mail.ru>
2025-07-05 17:34:54 +0300

    init: initial commit

---
+++ ishop0/App.js
@@ -0,0 +1,15 @@
+﻿import React from "react";
+import ReactDOM from "react-dom";
+
+import ShopInfo from "./components/Shop";
+
+ReactDOM.render(
+  <ShopInfo
+    name="Gourmet gifts for any occasion"
+    address="123 Main St, Anytown, USA"
+    phone="phone: 123-456-7890"
+    email="email: gourmet@e.com"
+    website="website: www.gourmet.com"
+  />,
+  document.getElementById("container")
+);
---
+++ ishop0/components/Shop.js
@@ -0,0 +1,21 @@
+﻿import React from "react";
+
+import "./Shop.css";
+
+class ShopInfo extends React.Component {
+  render() {
+    return (
+      <div className="Shop_Banner">
+        <p className="Shop_Name">
+          <strong>{this.props.name}</strong>
+        </p>
+        <p className="Shop_Address">{this.props.address}</p>
+        <p className="Shop_Phone">{this.props.phone}</p>
+        <p className="Shop_Email">{this.props.email}</p>
+        <p className="Shop_Website">{this.props.website}</p>
+      </div>
+    );
+  }
+}
+
+export default ShopInfo;
